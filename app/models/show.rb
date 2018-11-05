c6bc2f5e12fc4406b5a16b2e094e792cca7c48e6
Marc Mills <mdmills1990@gmail.com>
2018-11-05 17:02:35 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,11 +1,10 @@
 class Show < ActiveRecord::Base
 
-  def highest_rating
+  def self.highest_rating
     Show.maximum(:rating)
-    
   end
 
-  def most_popular_show
+  def self.most_popular_show
   end
 
   def lowest_rating
